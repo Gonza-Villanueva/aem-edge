010ca322f5966d37d6e14b967da4ef7850c151f5
Gonzalo Villanueva <gonzalo.villanueva@omni.pro>
2025-03-14 15:29:57 -0300

    feat: main clean console.log's

--- blocks/ulta-article-list/ulta-article-list.js
+++ blocks/ulta-article-list/ulta-article-list.js
@@ -1,3 +1,3 @@
-export default function decorate(block) {
-  console.log('block', block);
+export default function decorate() {
+  // console.log('block', block);
 }
--- blocks/ulta-title/ulta-title.js
+++ blocks/ulta-title/ulta-title.js
@@ -1,3 +1,3 @@
-export default function decorate(block) {
-  console.log(`${block} block here`);
+export default function decorate() {
+  // console.log(`${block} block here`);
 }
