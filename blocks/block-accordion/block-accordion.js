ffebab2fcb7d8614b505c73b86cf70b434905eee
Gonzalo Villanueva <gonzalo.villanueva@omni.pro>
2025-03-27 18:27:56 -0300

    feat: main block-accordion JS

--- blocks/block-accordion/block-accordion.js
+++ blocks/block-accordion/block-accordion.js
@@ -1,3 +1,39 @@
+import { h, render } from '@dropins/tools/preact.js';
+import htm from '../../scripts/htm.js';
+
+const html = htm.bind(h);
+const accordionBlock = ({ accordionClass, accordionType, accordionTextUl }) => html`
+  <div class="accordion ${accordionClass} ${accordionType}">
+    <div class="accordion__content" dangerouslySetInnerHTML=${{ __html: accordionTextUl.outerHTML }}></div>
+  </div>
+`;
+
 export default async function decorate(block) {
-  console.log('block-accordion', block);
+  const items = Array.from(block.children);
+  const accordionClass = items.shift().textContent.trim().toLowerCase().replace(/,/g, ' ');
+  const accordionType = items.shift().textContent.trim().toLowerCase().replace(/,/g, ' ');
+  const accordionTextHtml = items.shift();
+  const accordionTextelement = accordionTextHtml.querySelector('ul');
+  const accordionTextUl = accordionTextelement ? accordionTextelement.cloneNode(true) : null;
+
+  block.innerHTML = '';
+
+  const blockWrapperCloses = block.closest('.block-accordion-wrapper');
+  const heading = blockWrapperCloses?.previousElementSibling?.querySelector('h1, h2, h3, h4, h5, h6');
+  if (heading) {
+    heading.classList = 'accordion__heading';
+    heading.addEventListener('click', () => {
+      blockWrapperCloses.classList.toggle('open');
+    });
+  }
+
+  const app = html`
+  <${accordionBlock}
+  block=${block}
+  accordionClass=${accordionClass}
+  accordionType=${accordionType}
+  accordionTextUl=${accordionTextUl}
+  />`;
+
+  render(app, block);
 }
