68b12d73cbf292de9b9544584ea43e8f1ac00b49
Gonzalo Villanueva <gonzalo.villanueva@omni.pro>
2025-03-31 11:16:55 -0300

    feat: main block-image

--- blocks/block-image/block-image.js
+++ blocks/block-image/block-image.js
@@ -1,40 +1,39 @@
+/**
+ * @author    infinite Team
+ * @copyright Copyright(c) 2025 infinite
+ * @module    block/block-accordion
+ */
+
+import { h, render } from '@dropins/tools/preact.js';
+import htm from '../../scripts/htm.js';
+
+const html = htm.bind(h);
+
+const imageBlock = ({ blockImageDesktop }) => html`
+  <picture>
+    <source type="image/webp" media="(max-width: 900px)" srcset="${blockImageDesktop}">
+    <img loading="lazy" src="${blockImageDesktop}" alt="">
+  </picture>
+  -----------
+`;
+
+function getSrcOnWebply(elem) {
+  let imgSrc = elem.querySelector('div picture img').getAttribute('src');
+  imgSrc = imgSrc.replace('format=png', 'format=webply');
+  return imgSrc;
+}
+
 export default async function decorate(block) {
-  console.log('Desktop image: ', block.dataset.desktopimage);
-  console.log('Mobile image: ', block.dataset.mobileimage);
+  const items = Array.from(block.children);
+  console.log(items);
+  const blockImageDesktop = getSrcOnWebply(items.shift());
+  console.log(blockImageDesktop);
 
-  const desktopImage = block.dataset.desktopimage || '';
-  const mobileImage = block.dataset.mobileimage || '';
-  const desktopAlt = block.dataset.desktopalt || '';
-  const mobileAlt = block.dataset.mobilealt || desktopAlt;
-  const desktopUrl = block.dataset.desktopurl || '';
-  const mobileUrl = block.dataset.mobileurl || '';
+  const app = html`
+  <${imageBlock}
+  block=${block}
+  blockImageDesktop=${blockImageDesktop}
+  />`;
 
-  const picture = document.createElement('picture');
-
-  if (mobileImage) {
-    const source = document.createElement('source');
-    source.setAttribute('media', '(max-width: 900px)');
-    source.setAttribute('srcset', mobileImage);
-    picture.appendChild(source);
-  }
-
-  const img = document.createElement('img');
-  img.setAttribute('src', desktopImage || mobileImage || '');
-  img.setAttribute('alt', window.innerWidth <= 900 ? mobileAlt : desktopAlt);
-  picture.appendChild(img);
-
-  const wrapper = desktopUrl || mobileUrl
-    ? document.createElement('a')
-    : document.createElement('div');
-
-  wrapper.classList.add('block-image-wrapper');
-  if (window.innerWidth <= 900 && mobileUrl) {
-    wrapper.setAttribute('href', mobileUrl);
-  } else if (desktopUrl) {
-    wrapper.setAttribute('href', desktopUrl);
-  }
-
-  wrapper.appendChild(picture);
-  block.textContent = '';
-  block.appendChild(wrapper);
+  render(app, block);
 }
