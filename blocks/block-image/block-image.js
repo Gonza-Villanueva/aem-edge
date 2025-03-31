4711b6c7246cf41bbab18afea3e01779e0cd670a
Gonzalo Villanueva <gonzalo.villanueva@omni.pro>
2025-03-31 14:30:39 -0300

    feat: main block-image

--- blocks/block-image/block-image.js
+++ blocks/block-image/block-image.js
@@ -1,7 +1,7 @@
 /**
  * @author    infinite Team
  * @copyright Copyright(c) 2025 infinite
- * @module    block/block-accordion
+ * @module    block/block-image
  */
 
 import { h, render } from '@dropins/tools/preact.js';
@@ -9,14 +9,20 @@ import htm from '../../scripts/htm.js';
 
 const html = htm.bind(h);
 
-const imageBlock = ({ blockImageDesktop, blockImageMobile }) => html`
-  <picture>
-    ${blockImageMobile ? html`<source type="image/webp" media="(min-width: 900px)" srcset="${blockImageDesktop}">` : ''}
-    <source type="image/webp" srcset="${blockImageDesktop}">
-    <img loading="lazy" src="${blockImageDesktop}" alt="">
-  </picture>
-  -----------
+const imageBlock = ({ blockImageDesktop, blockImageMobile, blockAltImage }) => html`
+    <picture>
+      ${blockImageMobile ? html`
+        <source type="image/webp" media="(min-width: 900px)" srcset="${blockImageMobile}" />` : html`
+        <source type="image/webp" media="(min-width: 900px)" srcset="${blockImageDesktop}" />`}
+      <source type="image/webp" srcset="${blockImageDesktop}" />
+      <img loading="lazy" src="${blockImageDesktop}" alt="${blockAltImage}" />
+    </picture>
 `;
+
+function isAImg(elem) {
+  const image = elem.querySelector('div picture img');
+  return image;
+}
 
 function getSrcOnWebply(elem) {
   let imgSrc = elem.querySelector('div picture img').getAttribute('src');
@@ -24,18 +30,53 @@ function getSrcOnWebply(elem) {
   return imgSrc;
 }
 
+function getTextContent(el) {
+  return el?.textContent?.trim().toLowerCase().replace(/,/g, ' ') || '';
+}
+
+function isAHref(elem) {
+  const href = elem.querySelector('div a');
+  return href;
+}
+
+function getHrefFromButton(elem) {
+  const butonHref = elem.querySelector('div a').getAttribute('href');
+  return butonHref;
+}
+
 export default async function decorate(block) {
   const items = Array.from(block.children);
-  console.log(items);
   const blockImageDesktop = getSrcOnWebply(items.shift());
+
+  let blockImageMobile = items.shift();
+  if (isAImg(blockImageMobile) !== null) {
+    blockImageMobile = getSrcOnWebply(blockImageMobile);
+  } else {
+    blockImageMobile = false;
+  }
+
+  const blockAltImage = getTextContent(items.shift());
+
+  let blockHref = items.shift();
+  if (isAHref(blockHref) !== null) {
+    blockHref = getHrefFromButton(blockHref);
+  } else {
+    blockHref = false;
+  }
+
   console.log(blockImageDesktop);
-  const blockImageMobile = getSrcOnWebply(items.shift());
+  console.log(blockImageMobile);
+  console.log(blockAltImage);
+  console.log(blockHref);
+
+  block.innerHTML = '';
 
   const app = html`
   <${imageBlock}
   block=${block}
   blockImageDesktop=${blockImageDesktop}
   blockImageMobile=${blockImageMobile}
+  blockAltImage=${blockAltImage}
   />`;
 
   render(app, block);
