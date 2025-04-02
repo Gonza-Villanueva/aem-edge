7cf0b47863576b7d911af98899f31068b90ca98c
Gonzalo Villanueva <gonzalo.villanueva@omni.pro>
2025-04-02 14:32:28 -0300

    feat: main block-title

--- blocks/block-title/block-title.js
+++ blocks/block-title/block-title.js
@@ -4,6 +4,56 @@
  * @module    block/block-title
  */
 
+import { h, render } from '@dropins/tools/preact.js';
+import htm from '../../scripts/htm.js';
+
+const html = htm.bind(h);
+
+const titleBlock = ({
+  blocktitle,
+  blocktitleForId,
+  blocktype,
+  blockspan,
+}) => html`
+  ${blockspan === 'true'
+    ? html`<span><${blocktype} id="${blocktitleForId}">${blocktitle}</${blocktype}></span>`
+    : html`<${blocktype} id="${blocktitleForId}">${blocktitle}</${blocktype}>`}
+`;
+
+function getTextContent(el) {
+  return el?.textContent?.trim() || '';
+}
+
+function getSlugFromText(el) {
+  return el?.toLowerCase().replace(/\s+/g, '-') || '';
+}
+
 export default async function decorate(block) {
-  console.log(block);
+  const items = Array.from(block.children);
+  const blockName = getTextContent(items.shift());
+  const blocktitle = getTextContent(items.shift());
+  const blocktitleForId = getSlugFromText(blocktitle);
+  const blocktype = getTextContent(items.shift());
+  const blockspan = getTextContent(items.shift());
+
+  // temp block
+  const tempWrapper = document.createElement('div');
+
+  const app = html`
+  <${titleBlock}
+  blockName=${blockName}
+  blocktitle=${blocktitle}
+  blocktitleForId=${blocktitleForId}
+  blocktype=${blocktype}
+  blockspan=${blockspan}
+  />`;
+
+  // Render in the temporary wrapper
+  render(app, tempWrapper);
+
+  // Insert the content before the original block
+  block.parentNode.insertBefore(tempWrapper.firstElementChild, block);
+
+  // remove block
+  block.remove();
 }
