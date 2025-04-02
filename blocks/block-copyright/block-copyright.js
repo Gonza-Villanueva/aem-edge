338439f1aed0cfd5544eb76ab8b4edb50a0ba89d
Gonzalo Villanueva <gonzalo.villanueva@omni.pro>
2025-04-02 10:09:13 -0300

    feat: main footer styles

---
+++ blocks/block-copyright/block-copyright.js
@@ -0,0 +1,9 @@
+/**
+ * @author    infinite Team
+ * @copyright Copyright(c) 2025 infinite
+ * @module    block/block-copyright
+ */
+
+export default async function decorate(block) {
+  console.log(block);
+}
